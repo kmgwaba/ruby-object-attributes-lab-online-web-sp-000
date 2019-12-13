477a323ec31d6c351b36cbaa891368c3191d43f1
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-13 00:45:30 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+end
